cf336cd8b90ef69bb99b7b7866ec209ea62c9db0
Collin Gordon <colntrev@gmail.com>
2018-02-17 13:09:50 -0800

    Obfuscated file paths

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,6 +8,9 @@ import scala.Tuple2;
 
 import java.util.Arrays;
 
+import static main.java.Paths.INPUT_PATH;
+import static main.java.Paths.OUTPUT_PATH;
+
 /**
  * Created by colntrev on 2/17/18.
  */
@@ -15,13 +18,13 @@ public class Main {
     public static void main(String[] args){
         SparkConf conf = new SparkConf().setMaster("local").setAppName("Word Count");
         JavaSparkContext javaConf = new JavaSparkContext(conf);
-        JavaRDD<String> textFile = javaConf.textFile("src/main/resources/text.txt");
+        JavaRDD<String> textFile = javaConf.textFile(INPUT_PATH);
         JavaPairRDD<String,Integer> counts = textFile
                 .flatMap(s -> Arrays.asList(s.split("[ ,]")).iterator())
                 .mapToPair(word -> new Tuple2<>(word, 1))
                 .reduceByKey((a,b) -> a + b);
         counts.foreach(p -> System.out.println(p));
         System.out.println("Total Words: " + counts.count());
-        counts.saveAsTextFile("src/main/resources/textcount.txt");
+        counts.saveAsTextFile(OUTPUT_PATH);
     }
 }
